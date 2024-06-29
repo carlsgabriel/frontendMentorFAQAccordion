bdd5f24f1f543aac94f7a57aa3859c284f4e6513
carlsgabriel <gomescarlosleite@gmail.com>
2024-06-29 13:50:01 -0300

    fixing structure and javascript

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const buttons = document.querySelectorAll(".faq-ask img");
+const asks = document.querySelectorAll(".faq-ask span");
 const responses = document.querySelectorAll(".faq-response");
 
 
@@ -15,7 +16,20 @@ buttons.forEach((value, index) => {
             resp.classList.toggle("hidden");
             button.setAttribute("src", "assets/images/icon-plus.svg");
         }
-
-
     })
+});
+
+    asks.forEach((value, index) => {
+        value.addEventListener("click", () => {
+            let button = buttons[index];
+            let resp = responses[index];
+    
+            if(resp.classList.contains("hidden")){
+                resp.classList.toggle("hidden");
+                button.setAttribute("src", "assets/images/icon-minus.svg");
+            } else {
+                resp.classList.toggle("hidden");
+                button.setAttribute("src", "assets/images/icon-plus.svg");
+            }
+        })
 });
