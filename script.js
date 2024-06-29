7aa9760300e6d1a96a66032c24a0b9582a4b52a7
carlsgabriel <gomescarlosleite@gmail.com>
2024-06-29 13:25:42 -0300

    adding structure, styling, responsiveness, javascript and deleting unnecessary files

---
+++ script.js
@@ -0,0 +1,21 @@
+const buttons = document.querySelectorAll(".faq-ask img");
+const responses = document.querySelectorAll(".faq-response");
+
+
+
+buttons.forEach((value, index) => {
+    value.addEventListener("click", () => {
+        let button = buttons[index];
+        let resp = responses[index];
+
+        if(resp.classList.contains("hidden")){
+            resp.classList.toggle("hidden");
+            button.setAttribute("src", "assets/images/icon-minus.svg");
+        } else {
+            resp.classList.toggle("hidden");
+            button.setAttribute("src", "assets/images/icon-plus.svg");
+        }
+
+
+    })
+});
